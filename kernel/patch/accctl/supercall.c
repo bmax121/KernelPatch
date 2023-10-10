43ef371ddc2f8e6d712c8d20d74f50e0bf3cfa5f
bmax <bmax@placeholder.com>
2023-10-10 18:48:19 +0800

    typo

--- kernel/patch/accctl/supercall.c
+++ kernel/patch/accctl/supercall.c
@@ -94,7 +94,7 @@ static long supercall(long cmd, void *__user arg1, void *__user arg2, void *__us
 #ifdef ANDROID
         ret = supercall_android(cmd, arg1, arg2, arg3);
 #else
-        ret = SUPERCALL_RES_NOT_IMPL
+        ret = SUPERCALL_RES_NOT_IMPL;
 #endif
     }
     return ret;
