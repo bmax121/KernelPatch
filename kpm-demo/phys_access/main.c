0e5ddb9cc15ed601f5dc16c525bc3540da74a33e
bmax <bmax@placeholder.com>
2024-02-19 10:13:01 +0800

    x

--- kpm-demo/phys_access/main.c
+++ kpm-demo/phys_access/main.c
@@ -14,27 +14,20 @@ KPM_NAME("phys-access");
 KPM_VERSION("1.0.0");
 KPM_LICENSE("GPL v2");
 KPM_AUTHOR("bmax121");
-KPM_DESCRIPTION("Expose APIs for accessing process physical memory.");
+KPM_DESCRIPTION("Expose APIs for access process physical memory.");
 
 static int init(const char *args, void *__user reserved)
 {
-    pr_info("kpm hello init, args: %s\n", args);
-    pr_info("kernelpatch version: %x\n", kpver);
     return 0;
 }
 
 static int control(const char *args, char *__user out_msg, int outlen)
 {
-    pr_info("kpm hello control, args: %s\n", args);
-    char echo[64] = "echo: ";
-    strncat(echo, args, 48);
-    seq_copy_to_user(out_msg, echo, sizeof(echo));
     return 0;
 }
 
 static int exit(void *__user reserved)
 {
-    pr_info("kpm hello exit\n");
     return 0;
 }
 
