fd2d5f719153acffbd84ec4c83958c0084b0991e
Admirepowered <admire1971@outlook.com>
2026-02-02 23:24:39 +0800

    support v1 v2 kernel dtb

--- tools/bootimg.h
+++ tools/bootimg.h
@@ -44,6 +44,20 @@ typedef struct {
      uint8_t magic[8];
 } compress_head;
 
+#define DTB_MAGIC "\xd0\x0d\xfe\xed"
+
+struct fdt_header {
+    uint32_t magic;
+    uint32_t totalsize;
+    uint32_t off_dt_struct;
+    uint32_t off_dt_strings;
+    uint32_t off_mem_rsvmap;
+    uint32_t version;
+    uint32_t last_comp_version;
+    uint32_t boot_cpuid_phys;
+    uint32_t size_dt_strings;
+    uint32_t size_dt_struct;
+};
 
 int repack_bootimg(const char *orig_boot_path, 
                         const char *new_kernel_path, 
